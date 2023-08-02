1ac9124667f12540de6061c4d3b3413b88867048
Ronan Adriel Zenatti <ronan.zenatti@etec.sp.gov.br>
2023-08-01 22:24:30 -0300

    Aula 1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'helloWorld',
+  appName: 'Hello World',
   webDir: 'www',
   server: {
     androidScheme: 'https'
